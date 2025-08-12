fe838e1db0019afe5df4ad59272089be112d92e2
Dmitry Lesin <lesindima.official@gmail.com>
2025-08-12 18:45:42 +0300

    Add files via upload

---
+++ youtube-no-recommendations/content.js
@@ -0,0 +1,28 @@
+function hideRecommendations() {
+  const selectors = [
+    '#related',
+    '#secondary', 
+    'ytd-watch-next-secondary-results-renderer', 
+    'ytd-rich-grid-renderer',
+    'ytd-rich-item-renderer',
+    'ytd-compact-video-renderer',
+    '.ytd-recommended-video-renderer'
+  ];
+
+  selectors.forEach(selector => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(el => {
+      if (el) {
+        el.style.display = 'none';
+      }
+    });
+  });
+}
+
+hideRecommendations();
+
+const observer = new MutationObserver(hideRecommendations);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
