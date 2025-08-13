29e29948a83686dc8d0ab649ec215dfa0c851046
Dmitry Lesin <lesindima.official@gmail.com>
2025-08-13 09:06:24 +0300

    Update content.js

--- youtube-no-recommendations/content.js
+++ youtube-no-recommendations/content.js
@@ -1,13 +1,22 @@
 function hideRecommendations() {
   const selectors = [
     '#related',
-    '#secondary', 
-    'ytd-watch-next-secondary-results-renderer', 
+    '#secondary',
+    'ytd-watch-next-secondary-results-renderer',
     'ytd-rich-grid-renderer',
     'ytd-rich-item-renderer',
     'ytd-compact-video-renderer',
     '.ytd-recommended-video-renderer',
-    '#comments'
+    '#comments',
+
+    '.ytp-ad-module',
+    '#player-ads',
+    '.ytd-companion-slot-renderer',
+    '.ytd-action-companion-ad-renderer',
+    '.ytd-player-legacy-desktop-watch-ads-renderer', 
+    '.ytp-ad-player-overlay',
+    '.ytp-ad-skip-button',
+    '.ytp-ad-text'
   ];
 
   selectors.forEach(selector => {
@@ -27,4 +36,3 @@ observer.observe(document.body, {
   childList: true,
   subtree: true
 });
-
